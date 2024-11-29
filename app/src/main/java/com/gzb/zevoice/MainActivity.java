1671c47c220d7941fbeffe44413e7ff530a1aabb
gaspardzebear <ubu@LAPTOP-V5BH8JC1.localdomain>
2024-11-29 11:50:24 +0100

    WIP : handler. Bug ?? File creation

--- app/src/main/java/com/gzb/zevoice/MainActivity.java
+++ app/src/main/java/com/gzb/zevoice/MainActivity.java
@@ -12,6 +12,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import android.content.SharedPreferences;
 import android.media.PlaybackParams;
+import android.media.SoundPool;
 import android.net.Uri;
 import android.os.Bundle;
 import android.media.AudioRecord;
@@ -28,6 +29,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 //import android.support.v7.app.*;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.os.ParcelFileDescriptor;
 import android.os.SystemClock;
 import android.view.View;
@@ -37,6 +41,7 @@ import android.content.Intent;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -56,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean isRecording = false;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private static final int REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 201;
+    private static final int PCMBUFFER = 1;
 
     private static final int SAMPLE_RATE = 44100;
     private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
@@ -84,14 +90,18 @@ public class MainActivity extends AppCompatActivity {
     private Context context;
     private ActivityResultLauncher<Intent> launcher;
     String fName;
-    float pitch;
-    float speed;
+    private volatile float pitch;
+    private volatile float speed;
 
 
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted;
     private boolean permissionToWriteAccepted;
     private String[] permissions;
+
+    private SoundPool soundPool;
+
+    public AudioFeeder audioFeeder ;
 
     //-------------------------------------------------------------------------------------
     @Override
@@ -124,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
         // Requesting permission to RECORD_AUDIO
         permissionToRecordAccepted = false;
         permissionToWriteAccepted = false;
+        context=context;
 
         String[] permissionWriteExternalStorage = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
@@ -136,23 +147,12 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, permissionRecordAudio, REQUEST_RECORD_AUDIO_PERMISSION);
             //return;
         }
-        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-        //    Log.d("MAIN", "Request perm Write external storage");
-        //    ActivityCompat.requestPermissions(this, permissionWriteExternalStorage, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION);
-        //return;
-        // }
-
         if (!permissionToRecordAccepted) {
             Log.d("MAIN", "Record not allowed");
             //finish();
         }
-        //if (!permissionToWriteAccepted) {
-        //    Log.d("MAIN", "Write not allowed");
-        //finish();
-        //}
 
         //-------------------------------------------------------------------------------------------------------------
-
         Log.d("MAIN", "audiorecord Build");
         audioRecord = new AudioRecord.Builder()
                 .setAudioSource(AudioSource.MIC)
@@ -180,6 +180,22 @@ public class MainActivity extends AppCompatActivity {
                 .setTransferMode(AudioTrack.MODE_STREAM)
                 .build();
         Log.d("MAIN", "audiotrack Built");
+
+        AudioAttributes audioAttributes = new AudioAttributes
+                .Builder()
+                .setUsage( AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(  AudioAttributes .CONTENT_TYPE_SONIFICATION)
+                .build();
+        soundPool = new SoundPool
+                .Builder()
+                .setMaxStreams(3)
+                .setAudioAttributes( audioAttributes)
+                .build();
+
+        audioFeeder = new AudioFeeder(audioTrack);
+        Thread tAudioFeeder = new Thread(audioFeeder,"AudioFeeder");
+        tAudioFeeder.start();
+
 
         Log.d("MAIN", "listener Build");
         pitch05.setOnClickListener(new View.OnClickListener() {
@@ -268,10 +284,7 @@ public class MainActivity extends AppCompatActivity {
                 //playRecording();
             }
         });
-
-
         Log.d("LISTENER", "Built");
-
         launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), (result) -> {
                 if (result.getResultCode() == Activity.RESULT_OK) {
                     baseDocumentTreeUri = Objects.requireNonNull(result.getData()).getData();
@@ -280,7 +293,6 @@ public class MainActivity extends AppCompatActivity {
                     // take persistable Uri Permission for future use
                     //context.getContentResolver().takePersistableUriPermission(result.getData().getData(), takeFlags);
                     getContentResolver().takePersistableUriPermission(baseDocumentTreeUri, takeFlags);
-
                     //SharedPreferences preferences = context.getSharedPreferences("com.example.geofriend.fileutility", Context.MODE_PRIVATE);
                     //preferences.edit().putString("filestorageuri", result.getData().getData().toString()).apply();
                 } else {
@@ -289,7 +301,6 @@ public class MainActivity extends AppCompatActivity {
         });
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
         launcher.launch(intent);
-
     }
 
     //--------------------------------------------------------------------------------------------------------------
@@ -307,7 +318,6 @@ public class MainActivity extends AppCompatActivity {
         pbp.allowDefaults();
         pbp.setPitch(pitch);
         pbp.setSpeed(speed);
-
         audioTrack.setPlaybackParams(pbp);
         audioTrack.play();
         isRecording = true;
@@ -317,11 +327,9 @@ public class MainActivity extends AppCompatActivity {
             Log.d("MAIN", "recording thread start");
             while (isRecording) {
                 int read = audioRecord.read(audioBuffer, 0, audioBuffer.length);
-                //Log.d("MAIN", "got data len=" + String.valueOf(read));
                 if (read > 0) {
-                    //Log.d("MAIN", "writing");
-                    //Log.v("AUDIORECORD","writing");
-                    audioTrack.write(audioBuffer, 0, read, AudioTrack.WRITE_BLOCKING);
+                    //audioTrack.write(audioBuffer, 0, read, AudioTrack.WRITE_BLOCKING);
+                    audioFeeder.getHandler().obtainMessage(MainActivity.PCMBUFFER, read, -1, audioBuffer).sendToTarget();
                 }
             }
             Log.d("MAIN", "recording thread end");
@@ -372,8 +380,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //-------------------------------------------------------------------------------------
-    private void playRecordingWithFile(String fName) {
-        Log.d("MAIN", "playRecordingOnly()");
+    private void playRecordingWithFile(File file) {
+        Log.d("MAIN", "playRecordingOnly() " + " pitch=" + Float.toString(pitch) + " speed=" + Float.toString(speed));
         byte[] audioBuffer = new byte[bufferSize];
         PlaybackParams pbp = audioTrack.getPlaybackParams();
         pbp.allowDefaults();
@@ -381,16 +389,19 @@ public class MainActivity extends AppCompatActivity {
         pbp.setSpeed(speed);
         audioTrack.setPlaybackParams(pbp);
         audioTrack.play();
+
         try {
-            BufferedInputStream bis =
-                    new BufferedInputStream(new FileInputStream(new File(fName)));
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
             int read;
             while ( (read = bis.read(audioBuffer,0,bufferSize)) != -1) {
-                //Log.d("MAIN", "got data len=" + String.valueOf(read));
-                if (read > 0) {
-                    audioTrack.write(audioBuffer, 0, read, AudioTrack.WRITE_BLOCKING);
-                }
+                audioTrack.write(audioBuffer, 0, read, AudioTrack.WRITE_BLOCKING);
+                //audioFeeder.getHandler().obtainMessage(MainActivity.PCMBUFFER, read, -1, audioBuffer).sendToTarget();
             }
+            bis.close();
+            //SystemClock.sleep(2000);
+            audioTrack.flush();
+            audioTrack.pause();
+            audioTrack.stop();
         } catch (IOException e) {
             Log.d("MAIN","IOException while playRecording");
         }
@@ -398,39 +409,115 @@ public class MainActivity extends AppCompatActivity {
 
 
     //======================================================================================================================
+    private class AudioFeeder implements Runnable {
+
+        Handler handler;
+        private AudioTrack audioTrack;
+
+        public AudioFeeder(AudioTrack audioTrack) {
+            this.audioTrack=audioTrack;
+        }
+        public Handler getHandler() {
+             return(handler);
+        }
+        @Override
+        public void run() {
+             Looper.prepare();
+             handler = new Handler(Looper.myLooper()) {
+                 @Override
+                 public void handleMessage(Message msg) {
+                     switch (msg.what) {
+                         case PCMBUFFER:
+                             Log.d("AudioFeeder", "PCMBUFFER");
+                             int msgLength = msg.arg1;
+                             //byte[] buffer = new byte[msg.obj.toString().length()];
+                             synchronized ("PCM") {
+                                 Log.d("AudioFeeder", "PCMBUFFER writing");
+                                 audioTrack.write((byte[]) msg.obj, 0, msgLength, AudioTrack.WRITE_BLOCKING);
+                                 Log.d("AudioFeeder", "PCMBUFFER end of writing");
+                             }
+                             break;
+                         default:
+                             Log.d("AudioFeeder", "Unknown what");
+                             break;
+                     }
+                 }
+             };
+             Looper.loop();
+        }
+    }
+
+
+
+     //======================================================================================================================
     private class RecordingRunnable implements Runnable {
 
         //-------------------------------------------------------------------------------------
         // Records to a file then plays it
+        public int counter;
+
+        public RecordingRunnable() {
+            startWatchdog(2000);
+        }
+
+        public void startWatchdog(int interval) {
+            new Thread(() -> {
+                while (letAppRun.get() ) {
+                    recordingInProgress.set(false);
+                    Log.d("Watchdog", "recordingInProgress " + recordingInProgress + " counter " + counter);
+                    synchronized ("WATCHDOG") {
+                        SystemClock.sleep(interval);
+                    }
+                    recordingInProgress.set(true);
+                    SystemClock.sleep(interval);
+                    Log.d("Watchdog", "recordingInProgress " + recordingInProgress);
+                }
+            }).start();
+        }
+
         @Override
         public void run() {
+            counter=0;
             while (letAppRun.get() ) {
                 recordingInProgress.set(true);
-                new Thread(() -> {
-                    SystemClock.sleep(1500);
-                    recordingInProgress.set(false);
-                }).start();
+                counter++;
+                if (counter > 2 ) {
+                    counter=0;
+                }
+                synchronized ("WATCHDOG") {}
 
-                Log.d("MAIN", " ExtStorage  " + Environment.getExternalStorageDirectory());
-                final File file = new File(Environment.getExternalStorageDirectory(), "/Documents/ZeVoice.pcm");
-                fName = Environment.getExternalStorageDirectory() + "/Documents/ZeVoice.pcm";
-                Log.d("MAIN", "Recording File : " + fName);
+                File root = android.os.Environment.getExternalStorageDirectory();
+                File dir = new File (root.getAbsolutePath() + "/Documents");
+                dir.mkdirs();
+                File file = new File(dir, "/ZeVoice/ZeVoice.pcm");
+
+                //String fSuffix="/Documents/ZeVoice.pcm";
+                //File dir=Environment.getExternalStorageDirectory();
+                //final File file = new File(dir, fSuffix);
+                //String fName="ZeVoice;";
+                //final DocumentFile dFile = DocumentFile.fromTreeUri(context,baseDocumentTreeUri).createFile("audio/pcm",fName);
+                //fName = dir + fSuffix;
+                Log.d("MAIN", "Recording File : " + file.getName() );
                 final ByteBuffer buffer = ByteBuffer.allocateDirect(bufferSize);
-                try (final FileOutputStream outStream = new FileOutputStream(file)) {
+                try {
+                    //final FileOutputStream outStream = new FileOutputStream(dFile.getName());
+                    final FileOutputStream outStream = new FileOutputStream(file);
                     while (recordingInProgress.get()) {
                         int result = audioRecord.read(buffer, bufferSize);
-                        if (result < 0) {
-                            throw new RuntimeException("Reading of audio buffer failed: " +
-                                    getBufferReadFailureReason(result));
-                        }
                         outStream.write(buffer.array(), 0, bufferSize);
                         buffer.clear();
                     }
+                    outStream.flush();
+                    outStream.close();
+                    Log.d("MAIN", "Stopped by watchdog, closing");
                 } catch (IOException e) {
                     Log.d("MAIN", "Exception");
                     throw new RuntimeException("Writing of recorded audio failed", e);
                 }
-                playRecordingWithFile(fName);
+                SystemClock.sleep(10);
+                //recordingInProgress.set(false);
+                playRecordingWithFile(file);
+                //recordingInProgress.set(true);
             }
         }
 
